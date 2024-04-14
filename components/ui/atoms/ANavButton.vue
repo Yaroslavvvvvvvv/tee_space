bf02f4f10c6f0ef1f7a2bb1fc3dc254998dcf528
Yaroslav Kovalchuk <dantekip972@gmail.com>
2024-04-14 18:05:07 +0300

    starting to do header

---
+++ components/ui/atoms/ANavButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {defineProps} from 'vue';
+
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <button class="bg-black text-white text-sm py-2 px-6 rounded-xl hover:opacity-80 transition">
+    {{ title }}
+  </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/ui/molecules/MNavbar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {defineProps} from 'vue';
+
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <a href="#" class="flex items-center justify-between w-full text-sm font-bold text-black rounded hover:text-primary transition">
+    {{ title }}
+    <slot></slot>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
